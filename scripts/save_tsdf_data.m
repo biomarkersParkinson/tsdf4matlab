602fa09c52fdef6a5ae53c531605ec1125d3c4d2
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2023-10-18 12:02:14 +0200

    Update the path to python scripts

--- scripts/save_tsdf_data.m
+++ scripts/save_tsdf_data.m
@@ -23,7 +23,7 @@ function [mat_succ] = save_tsdf_data(mat_list_metadata, mat_list_data, mat_dir_p
     
 
     % Run the python script load_legacy_tsdf_metadata_from_path.py and get the 3 outputs
-    [py_succ, py_list_metadata, py_list_data] = pyrunfile("python/load_legacy_tsdf_metadata_from_path.py",["Success" "Return_list_metadata" "Return_list_data"], py_list_metadata=py_list_metadata, py_list_data=py_list_data, py_dir_path=py_dir_path, py_metadata_file_name=py_metadata_file_name);
+    [py_succ, py_list_metadata, py_list_data] = pyrunfile("tsdf_python_interface\load_legacy_tsdf_metadata_from_path.py",["Success" "Return_list_metadata" "Return_list_data"], py_list_metadata=py_list_metadata, py_list_data=py_list_data, py_dir_path=py_dir_path, py_metadata_file_name=py_metadata_file_name);
 
     % Convert Python boolean to MATLAB logical
     mat_succ = logical(py_succ);
