c916fb14e687fd229c305f1cd8a133ebea7e3917
KarsVeldkamp <veldkampkars@gmail.com>
2024-03-11 15:33:31 +0100

    Fix tsdf_values_idx.m for other tsdf filenames

--- tsdf_values_idx.m
+++ tsdf_values_idx.m
@@ -1,8 +1,8 @@
-function indices = tsdf_values_idx(metadata_list, X, week)
+function indices = tsdf_values_idx(metadata_list, suffix)
     indices = [];
     for i = 1:length(metadata_list)
         % Use regular expression to match the pattern
-        expression = sprintf('WatchData\\.(PPG|IMU|EDA|ECG)\\.Week%s\\.raw_segment\\d+_%s\\.bin', num2str(week), X);
+        expression = ['**' suffix '.bin'];
         if ~isempty(regexp(metadata_list{i}.file_name, expression, 'once'))
             indices = [indices, i];
         end
